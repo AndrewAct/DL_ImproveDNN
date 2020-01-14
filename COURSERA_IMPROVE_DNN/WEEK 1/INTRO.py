16a6cfeac9fec938af944058c72420e5fa549789
Andrew Chen <43218650+AndrewBME@users.noreply.github.com>
2020-01-13 21:10:38 -0500

    A brief intro.

---
+++ COURSERA_IMPROVE_DNN/WEEK 1/INTRO.py
@@ -0,0 +1,9 @@
+## This course is offered by Coursera ##
+## Instructed by Andrew Ng ##
+## Its full name is ##
+
+## Improving Deep Neural Networks: ##
+## Hyperparameter tuning, Regularization and Optimization ##
+
+
+## Coded by Andrew Chen ##
