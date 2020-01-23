228a304beef02e7a0cf54c34b7773ea4c1e16d1e
Andrew Chen <43218650+AndrewBME@users.noreply.github.com>
2020-01-22 23:29:01 -0500

    The last (possibly) submission of WEEK 2 lectures.

--- COURSERA_IMPROVE_DNN/WEEK 2/Lecture.py
+++ COURSERA_IMPROVE_DNN/WEEK 2/Lecture.py
@@ -37,3 +37,51 @@
  # - Typical mini-batch size: 
  # Make sure batch size matches CPU/GPU memory
  
+ #### 1/21/20 ####
+
+# Exponentially Weighted Averages #
+# Example: Weather in London #
+# Vt = 0.9* V(t-1) + 0.1*theta(t)
+
+#  Or expressed in thsi form: V(t) = beta*V(t-1) + (1-beta)*theta(t)
+# Vt as approximately average over 1/(1-beta)* temperature
+
+# Understand Exponential Wighted Averages #
+# V(t) = beta*V(t-1) + (1-beta)*theta(t)
+# (1 - epsilon)^(1/epsilon) = 1/e
+
+# Bias Correction #
+# V(t)/(1- beta^t)
+
+# Gradient Descent with Momentum #
+# On iteration t
+# Compute dW, db on mini batch 
+
+# v[dW] = beta*v[dW] + (1-beta)*dW
+# v[db] = beta*v[db] + (1-beta)*db 
+# W = W - av[dW], b = b - alpha*v[db]
+
+# Note: alpha is the learning rate 
+
+# RMSprop #
+# Slow down or speed up leanring 
+# S[dW] = beta* S[dW]+ (1- beta)* dW^2
+# S[db] = beta*S[db] + (1-beta)* db^2
+ # Ensure the algorithm is not dividing zero! 
+
+# Adam Optimization #
+   # Adaptivie Moment Estimation
+ # Initizlize V[dW] = 0, S[dW] = 0, V[ab] = 0
+ # On iteration t:
+ # COmpute dW, db
+ # V[dW] = beta*V[dW] + (1-beta) *dW
+ # S[dW] = beta* S[dW]+ (1- beta)* dW^2
+ # S[db] = beta*S[db] + (1-beta)* db^2
+  # Correct Bias
+# V[dW]= V[dW]/(1-beta)
+
+# Learning rate Decay #
+
+# The Problem of Local Optima #
+# Saddle point and local optimum 
+# Plateu 
